3101b476663bf7e2584d77f37797a39009b7cce1
Benjah Senzele <senzelebenjamine@gmail.com>
2025-09-07 17:58:00 +0200

    add required project folders

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Rotate the propeller around its z-axis
+        transform.Rotate(0f, 0f, rotationSpeed);
+
+    }
+}
